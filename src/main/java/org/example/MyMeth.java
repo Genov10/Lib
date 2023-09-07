45142c0cf35be9b8be1cd77074057bdb27f70428
Genov10 <genovolexandr10@gmail.com>
2023-09-07 21:42:46 +0300

    add library for hw10

---
+++ src/main/java/org/example/MyMeth.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class MyMeth {
+
+        public static void sum(int a, int b) {
+            System.out.println(a + b);
+        }
+
+        public static void difference(int a, int b) {
+            System.out.println(a - b);
+        }
+
+        public static void multiply(int a, int b) {
+            System.out.println(a * b);
+        }
+
+        public static void divide(int a, int b) {
+            System.out.println(a / b);
+        }
+    }
